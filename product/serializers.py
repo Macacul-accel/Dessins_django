3a1667a599b10a63c5bd45d1cdb91e182ab28fe9
Leo LIU <leo_liu@hotmail.fr>
2025-01-13 17:28:21 +0100

    Search view

--- product/serializers.py
+++ product/serializers.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 from .models import Category, Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    
     class Meta:
         model = Product
         fields = (
             'id',
             'name',
+            'category_name',
             'get_absolute_url',
             'description',
             'price',
