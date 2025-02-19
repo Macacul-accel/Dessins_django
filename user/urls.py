b9777ebcfb8909fb6f3389af0b08cadaec4f4494
Leo LIU <leo_liu@hotmail.fr>
2025-02-19 16:55:43 +0100

    ajout views  after stripe redirect

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import logout, MyTokenObtainPairView, MyTokenRefreshView
+from .views import logout, MyTokenObtainPairView, MyTokenRefreshView, restore_cookies
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
+    path('restaure_cookies/', restore_cookies),
     path('jwt/create/', MyTokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', MyTokenRefreshView.as_view(), name='jwt-refresh'),
     path('logout/', logout),
